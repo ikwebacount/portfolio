f1e297c73da89e4b5c5da14e8352d9a1e7890eb3
ikwebacount <155137248+ikwebacount@users.noreply.github.com>
2024-03-13 19:14:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,71 +1,170 @@
-/* 要素を取得 */
-var a=document.getElementById("main2Item1Controle");
-var b=document.getElementById("main2Item2Controle");
-var c=document.getElementById("main2Item3Controle");
+/* 要素を取得と変数への格納*/
+var main2Item1Controle=document.getElementById("main2Item1Controle");
+var main2Item2Controle=document.getElementById("main2Item2Controle");
+var main2Item3Controle=document.getElementById("main2Item3Controle");
 var left=document.getElementById("leftBtn");
 var right=document.getElementById("rightBtn");
 
+/* 変数の定義 */
+var i=0
+
+/* 関数の定義 */
+ /* SNSの塊にクラスとｃｓｓを追加 */
 
 
-/* 変数、配列の定義 */
-var value=[
-    ["flex","none","none"],
-    ["none","block","none",],
-    ["none","none","block"]
-];
-
-var i=0;
-var x=0;
-
-a.style.display=value[x][0];
-b.style.display=value[x][1];
-c.style.display=value[x][2];
-
-/* ボタンのファンクション */
-leftBtn.onclick=function(){
-    i++;
-
-    console.log(i)
-
-    if(3-i>=0){
-        x=3-i;
-    }else if(3-i<0){
-        i=1;
-        x=3-i;
-    };
-    console.log(x)
-
-    a.style.display=value[x][0];
-    b.style.display=value[x][1];
-    c.style.display=value[x][2];
-
-    console.log(a.style.display=value[x][0]);
-    console.log(b.style.display=value[x][1]);
-    console.log(c.style.display=value[x][2]);
+function main2Item1ControlLeftTranslate(){
+    main2Item1Controle.classList.add("main2Item1AddClass")
 }
 
-rightBtn.onclick=function(){
-    i++;
 
-    console.log(i)
-    
-    if(i<3){
-        x=i;
-    }else if(3<=i){
-        i=0;
-        x=i;
-    };
-    console.log(x)
-
-    a.style.display=value[x][0];
-    b.style.display=value[x][1];
-    c.style.display=value[x][2];
-
-    console.log(a.style.display=value[x][0]);
-    console.log(b.style.display=value[x][1]);
-    console.log(c.style.display=value[x][2]);
+function main2Item1ControlRihgtTranslate(){
+    main2Item1Controle.classList.add("main2Item1AddClassRev")
 }
 
-/* 経過時間で画面が変わる */
+function main2Item1ControleOpasi(){
+    main2Item1Controle.classList.add("main2Item1AddClass2")
+}
 
-setInterval(rightBtn.onclick,5000)
+function main2Item1ControleReset(){
+    main2Item1Controle.classList.remove("main2Item1AddClass")
+    main2Item1Controle.classList.remove("main2Item1AddClassRev")
+    main2Item1Controle.classList.remove("main2Item1AddClass2")
+}
+
+
+
+ /* 依頼をしてみる */
+function main2Item2ControlLeftTranslate(){
+    main2Item2Controle.classList.add("main2Item2AddClass")
+}
+
+
+function main2Item2ControlRihgtTranslate(){
+    main2Item2Controle.classList.add("main2Item2AddClassRev")
+}
+
+function main2Item2ControleOpasi(){
+    main2Item2Controle.classList.add("main2Item2AddClass2")
+}
+
+function main2Item2ControleReset(){
+    main2Item2Controle.classList.remove("main2Item2AddClass")
+    main2Item2Controle.classList.remove("main2Item2AddClassRev")
+    main2Item2Controle.classList.remove("main2Item2AddClass2")
+}
+
+ /* 3つめのコンテンツ */
+function main2Item3ControlLeftTranslate(){
+    main2Item3Controle.classList.add("main2Item3AddClass")
+}
+
+
+function main2Item3ControlRihgtTranslate(){
+    main2Item3Controle.classList.add("main2Item3AddClassRev")
+}
+
+function main2Item3ControleOpasi(){
+    main2Item3Controle.classList.add("main2Item3AddClass2")
+}
+
+function main2Item3ControleReset(){
+    main2Item3Controle.classList.remove("main2Item3AddClass")
+    main2Item3Controle.classList.remove("main2Item3AddClassRev")
+    main2Item3Controle.classList.remove("main2Item3AddClass2")
+}
+
+/* 左の操作の関数 */
+function leftAction(){
+    i++
+
+    switch(i%3){
+        case 1:
+            main2Item1ControlLeftTranslate()
+            main2Item2ControleOpasi()
+            main2Item3ControleReset()
+            setTimeout(main2Item1ControleReset,500)
+            break
+        case 2:
+            main2Item2ControlLeftTranslate()
+            main2Item3ControleOpasi()
+            main2Item1ControleReset()
+            setTimeout(main2Item2ControleReset,500)
+            break
+        case 0:
+            main2Item3ControlLeftTranslate()
+            main2Item1ControleOpasi()
+            main2Item2ControleReset()
+            setTimeout(main2Item3ControleReset,500)
+            break
+    }
+}
+
+/* 右の操作の関数 */
+function rightAction(){
+    if(i<=0){
+        i=98
+    }else{i--}
+
+    switch(i%3){
+        case 1:  
+            main2Item3ControlRihgtTranslate()  
+            main2Item2ControleOpasi()  
+            main2Item1ControleReset()
+            setTimeout(main2Item3ControleReset,500)
+            break
+        case 2:
+            main2Item1ControlRihgtTranslate()          
+            main2Item3ControleOpasi() 
+            main2Item2ControleReset()  
+            setTimeout(main2Item1ControleReset,500)
+            break
+        case 0:
+            main2Item2ControlRihgtTranslate()  
+            main2Item1ControleOpasi()
+            main2Item3ControleReset()  
+            setTimeout(main2Item2ControleReset,500)              
+            break
+    }    
+}
+
+
+/* 初期値 */
+switch(i%3){
+    case 1:
+        main2Item1ControlLeftTranslate()
+        main2Item2ControleOpasi()
+        main2Item3ControleReset()
+        setTimeout(main2Item1ControleReset,500)
+        break
+    case 2:
+        main2Item2ControlLeftTranslate()
+        main2Item3ControleOpasi()
+        main2Item1ControleReset()
+        setTimeout(main2Item2ControleReset,500)
+        break
+    case 0:
+        main2Item3ControlLeftTranslate()
+        main2Item1ControleOpasi()
+        main2Item2ControleReset()
+        setTimeout(main2Item3ControleReset,500)
+        break
+
+}
+
+/* インターバル設定 */
+var interval=setInterval(leftAction,5000)
+
+/* 左の操作 */
+left.onclick=function(){
+    leftAction()
+    clearInterval(interval)
+    interval=setInterval(leftAction,5000)
+}
+
+/* 右の操作 */
+right.onclick=function(){
+    rightAction()
+    clearInterval(interval)
+    interval=setInterval(leftAction,5000)
+
+}
